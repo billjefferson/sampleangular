a8d6a6e856a2978458b3f619a97093a75bafc77c
billjefferson <bjmaclang@gmail.com>
2019-12-13 15:06:49 +0800

    add directive practice

---
+++ SampleAngular/customizedmodal/customizedmodal.js
@@ -0,0 +1,69 @@
+﻿var app = angular.module("Demo", ["ngAnimate"]);
+
+app.controller("AppController", function ($scope) {
+    $scope.alertSomething = function () {
+        
+    }
+})
+
+app.service("modals", function ($rootScope, $q) {
+    var modal = {
+        deferred: null,
+        params: null
+    };
+
+    return ({
+        open: open,
+        params: params,
+        reject: reject,
+        resolve: resolve
+    });
+
+    function open(type, params, pipeResponse) {
+        var previousDeferred = modal.deferred;
+
+        modal.deferred = $q.defer();
+        modal.params = params;
+
+        $rootscope.$emit("modals.open", type);
+
+        return (modal.deferred.promise);
+    }
+
+    function params() {
+        return (modal.params || {});
+    }
+
+    function reject(reason) {
+        if (!modal.deferred) {
+            return;
+        }
+
+        modal.deferred.reject(reason);
+
+        modal.deferred = modal.params = null;
+
+        $rootScope.$emit("modals.close");
+    }
+
+    function resolve(response) {
+        if (!modal.deferred) {
+            return;
+        }
+
+        modal.deferred.resolve(reponse);
+
+        modal.deferred = modal.params = null;
+
+        $rootScope.$emit("modals.close");
+    }
+});
+
+app.directive("bnModals", function ($rootScope, modals) {
+    return (link);
+
+    function link(scope, element, attributes) {
+
+    }
+});
+
---
+++ SampleAngular/directives/directives.js
@@ -0,0 +1,103 @@
+﻿//angular.module("app", [])
+//.controller("appCtrl", ["$scope", function ($scope) {
+//    $scope.customer = {
+//        name: "Naomi 1",
+//        address: "Ampitheater 1"
+//    }
+//}])
+//.directive("myCustomer", function () {
+//    return {
+//        template: "Name: {{customer.name}} Address: {{customer.address}}"
+//    }
+//})
+
+//angular.module("app", [])
+//.controller("appCtrl", ["$scope", function ($scope) {
+//    $scope.customer = {
+//        name: "Naomi 2",
+//        address: "Ampitheater 2"
+//    }
+//}])
+//.directive("myCustomer", function () {
+//    return {
+//        templateUrl: "new-directives.html"
+//    }
+//})
+
+//angular.module("app", [])
+//.controller("appCtrl", ["$scope", function ($scope) {
+//    $scope.customer = {
+//        name: "Naomi 2",
+//        address: "Ampitheater 2"
+//    }
+//}])
+//.directive("myCustomer", function () {
+//    return {
+//        templateUrl: function (element, attr) {
+//            return  attr.type + '.html';
+//        }
+//    }
+//})
+
+//angular.module("app", [])
+//.controller("appCtrl", ["$scope", function ($scope) {
+//    $scope.customer = {
+//        name: "Naomi 3",
+//        address: "Ampitheater 3"
+//    }
+//}])
+//.directive("myCustomer", function () {
+//    return {
+//        restrict: "E",
+//        templateUrl: "new-directives.html"
+//    };
+//});
+
+//angular.module("app", [])
+//.controller("appCtrl1", ["$scope", function ($scope) {
+//    $scope.customer = {
+//        name: "Naomi 4",
+//        address: "Ampitheater 4"
+//    }
+//}])
+//.controller("appCtrl2", ["$scope", function ($scope) {
+//    $scope.customer = {
+//        name: "Naomi 5",
+//        address: "Ampitheater 5"
+//    }
+//}])
+//.directive("myCustomer", function () {
+//    return {
+//        templateUrl: "new-directives.html"
+//    }
+//})
+
+angular.module("app", [])
+.controller("appCtrl", ["$scope", function ($scope) {
+    $scope.naomi = {
+        name: "Naomi 6",
+        address: "Ampitheater 6"
+    };
+    $scope.igor = {
+        name: "Igor",
+        address: "Somewhere else"
+    };
+
+}])
+.directive("myDirective", function () {
+    return {
+        restrict: "E",
+        //$scope.customerInfo=info="naomi"
+        //$scope.customerInfo=info="igor"
+        /*
+        scope: {
+          // same as '=customer'
+          customer: '='
+        },
+        */
+        scope: {
+            customerInfo: "=info"
+        },
+        templateUrl: "new-directives.html"
+    }
+})
